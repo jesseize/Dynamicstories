c9833d945c02ba3d8451830f2b4c50b43b1bfcc8
jesseiz <jesse@gmail.com>
2017-10-16 09:42:00 +0200

    Init

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- META & OTHER -->
+    <meta charset="utf-8">
+    <title>Dynamic Stories</title>
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,500,600,700,800,900" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <!-- LEFT BORDER -->
+      <div class="container__leftbar">
+        <div class="container__leftbar__menu">
+          <i class="fa fa-caret-right" aria-hidden="true"></i>
+        </div>
+      </div>
+      <!-- TOP BORDER -->
+      <div class="container__topbar">
+        <div class="container__topbar__logo"></div>
+      </div>
+      <!-- RIGHT BORDER -->
+      <div class="container__rightbar">
+        <ul class="container__rightbar__social">
+          <li class="container__rightbar__social__icon"><a href="config.facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+          <li class="container__rightbar__social__icon"><a href="config.instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+          <li class="container__rightbar__social__icon"><a href="config.twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+        </ul>
+      </div>
+      <!-- BOTTOM BORDER -->
+      <div class="container__bottombar">
+
+      </div>
+      <!-- CONTENT -->
+      <div class="container__content">
+        <section id="intro" class='container__content__intro'>
+          <div class="container__content__intro__content">
+            <div class="container__content__intro__content__title">
+              <h1 class="container__content__intro__content__title--top">HELLO</h1>
+              <!-- <h1 class="container__content__intro__content__title--bottom">LLO.</h1>-->
+            </div>
+
+          </div>
+          <div class="container__content__intro--left"></div>
+          <div class="container__content__intro--right"></div>
+        </section>
+      </div>
+    </div>
+    <!-- SCRIPTS -->
+    <script src="assets/js/app.js" charset="utf-8"></script>
+  </body>
+</html>
